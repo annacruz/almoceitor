8bee464063144a6461b17184878d97fefc5bb82d
Anna Cruz <anna.cruz@gmail.com>
2013-11-22 00:41:18 -0200

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.1'
+gem 'pg'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+gem 'rails-backbone'
+gem 'pony'
+
+group :development do
+ gem 'unicorn'
+ gem 'capistrano'
+ gem 'pry'
+ gem 'pry-rails'
+ gem 'pry-debugger'
+ gem 'mailcatcher'
+end
+
+group :test do
+ gem 'rspec'
+ gem 'capybara'
+ gem 'jasmine'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Almoceitor::Application.config.secret_key_base = 'aec10e0a1787e115e517d5a526eec6c1a23894b0c0abd969fe0174cbe2e4d41aaf1342939d28c2f36ebb7f8adc081d220275ffb33d09caeafbee6bd97359b4d0'
